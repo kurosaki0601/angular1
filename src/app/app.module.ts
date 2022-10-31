9ce4db52a56f7e1fecb68d1c3a1355e7d71a7fe5
Pham Huy Hoang <phamhuyhoang150@gmail.com>
2022-10-31 08:52:47 +0700

    create basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, MainComponent, FooterComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'todos',
+        component: MainComponent,
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
